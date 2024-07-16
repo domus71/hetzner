df125a034c6c76946f592ef87bbaec0502ee2e7c
Konstantinos Papadakis <domus71@gmail.com>
2024-07-16 18:33:23 +0300

    bugs

--- index.js
+++ index.js
@@ -7,28 +7,22 @@ const main = async () => {
   try {
     const result = await axios.get(config.myipbot);
     const myIP = result.data;
-
+    console.log("IP: ", myIP);
     const zones = await Client.Zones.GetAll(1, 10, config.domain);
     const zone = zones["zones"].find((z) => {
       return z.name === config.domain;
     });
 
-    const records = await Client.Records.GetAll(1, 10, zone.id);
+    const records = await Client.Records.GetAll(1, 25, zone.id);
+    // console.log(records);
     const record = records["records"].find((r) => {
       return r.name === config.host;
     });
 
-    const data = await Client.Records.Update(
-      record.id,
-      zone.id,
-      config.host,
-      "A",
-      myIP,
-      config.ttl
-    );
+    const data = await Client.Records.Update(record.id, zone.id, config.host, "A", myIP, config.ttl);
 
     const newRecord = await Client.Records.Get(record.id);
-    //console.log(newRecord);
+    console.log(newRecord);
   } catch (error) {
     console.log(error);
   }
